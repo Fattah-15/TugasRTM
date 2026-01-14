baeb4b7b8d22084fd5d199fdf037e0113ccb1d68
Fattah-15 <naufalfattah087@gmail.com>
2026-01-14 03:59:18 -1100

    Add files via upload

---
+++ users-music_app.sql
@@ -0,0 +1,9 @@
+USE music_app;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    email VARCHAR(100),
+    password VARCHAR(255),
+    full_name VARCHAR(100)
+);
